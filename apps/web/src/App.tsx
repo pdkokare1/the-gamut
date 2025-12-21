0490bd87347f2de39d2a67f99abf8d972c59356a
pdkokare1 <pdkokare1@gmail.com>
2025-12-21 13:49:13 +0530

    Update App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,8 @@ import { AppLayout } from "./components/layout/AppLayout";
 import { HomePage } from "./pages/HomePage";
 import { SearchPage } from "./pages/SearchPage";
 import { ProfilePage } from "./pages/ProfilePage";
+import { EmergencyPage } from "./pages/EmergencyPage";
+import { NarrativePage } from "./pages/NarrativePage";
 
 function App() {
   return (
@@ -12,7 +14,11 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/saved" element={<ProfilePage />} /> {/* Shortcut for now */}
+          <Route path="/saved" element={<ProfilePage />} />
+          
+          {/* New Routes */}
+          <Route path="/emergency" element={<EmergencyPage />} />
+          <Route path="/narrative/:id" element={<NarrativePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
