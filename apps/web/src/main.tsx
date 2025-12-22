b7b4b71cf8c7bc3632a80732fa24324f5eb2b1f9
pdkokare1 <pdkokare1@gmail.com>
2025-12-22 20:37:57 +0530

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Providers } from './components/Providers';
 import './index.css';
+
+// PWA: Register Service Worker
+import { registerSW } from 'virtual:pwa-register';
+
+// Automatically reload if a new version is available
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Providers>
-      <App />
-    </Providers>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
